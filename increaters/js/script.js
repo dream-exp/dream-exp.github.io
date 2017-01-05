556b906efbb6d6061924879134d0811fe360b130
Shunnosuke_Kataoka <shunnosuke834@gmail.com>
2017-01-05 17:40:17 +0900

    fix pages.

--- increaters/js/script.js
+++ increaters/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   console.log("test");
-  $('p.caption').animate({
-    left: '100px',
+  $('div.top-caption').animate({
+    left: '20px',
     opacity: 1.0
-  }, 1500);
+  }, 800);
 });
