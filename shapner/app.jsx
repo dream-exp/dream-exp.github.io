244b899d247d6626f4433bf46ac05537411dac38
Shunnosuke_Kataoka <shunnosuke834@gmail.com>
2017-07-20 17:05:14 +0900

    Add : Shapner.

---
+++ shapner/app.jsx
@@ -0,0 +1,61 @@
+class Translater extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputText: '',
+      outputText: '',
+    };
+
+    this.onChangeText = this.onChangeText.bind(this);
+    this.textShapner = this.textShapner.bind(this);
+    this.textClear = this.textClear.bind(this);
+
+    let clipboard = new Clipboard('.copy');
+      clipboard.on('success', function(e) {
+        console.info('Trigger:', e.trigger);
+      });
+      clipboard.on('error', function(e) {
+        console.error('Trigger:', e.trigger);
+      });
+  }
+
+  onChangeText(e) {
+    this.setState({ inputText: e.target.value });
+    this.textShapner();
+  }
+
+  textShapner() {
+    let text = this.state.inputText.replace(/-\s/g, "");
+    text = text.replace(/-\n/g, "")
+    text = text.replace(/\n/g, "");
+    this.setState({ outputText: text });
+    console.log(this.state.outputText);
+  }
+
+  textClear() {
+    this.setState({
+      inputText: '',
+      outputText: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea type="textarea" value={this.state.inputText} onChange={this.onChangeText} />
+        <div className="buttons">
+          <button className="clear" onClick={this.textClear}>Clear</button>
+          <button className="shaping" onClick={this.textShapner}>Shaping!</button>
+        <button className="copy" data-clipboard-target=".outputText" onClick={this.textClear}>Copy!</button>
+        </div>
+        <textarea className="outputText" value={this.state.outputText}/>
+      </div>
+    );
+  }
+}
+
+React.render(
+    <Translater />,
+    document.getElementById('app-container')
+);
