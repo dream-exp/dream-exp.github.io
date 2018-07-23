d3feaacdef2de61bafb8e55e381f075af6d990fc
Shunnosuke Kataoka <shunnosuke834@gmail.com>
2018-07-23 13:37:00 +0900

    クリエイティブキッズキャンプ用のトーナメント表示機能を公開

---
+++ tornament/script.js
@@ -0,0 +1,181 @@
+$('div.first-left').click(function() {
+	if($('div.first-left').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.first-left').css({
+			'borderColor':'red'
+		});
+		$('div.third-right').css({
+			'borderRight':'solid 5px red'
+		});
+	} else {
+		$('div.first-left').css({
+			'borderColor':'black'
+		});
+		$('div.third-right').css({
+			'borderRight':'solid 5px black'
+		});
+	}
+});
+
+$('div.first-right').click(function() {
+	if($('div.first-right').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.first-right').css({
+			'borderColor':'red'
+		});
+		$('div.third-right').css({
+			'borderRight':'solid 5px red'
+		});
+	} else {
+		$('div.first-right').css({
+			'borderColor':'black'
+		});
+		$('div.third-right').css({
+			'borderRight':'solid 5px black'
+		});
+	}
+});
+
+
+$('div.second-left').click(function() {
+	if($('div.second-left').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.second-left').css({
+			'borderColor':'red'
+		});
+		$('div.fourth-left').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.second-left').css({
+			'borderColor':'black'
+		});
+		$('div.fourth-left').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
+$('div.second-right').click(function() {
+	if($('div.second-right').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.second-right').css({
+			'borderColor':'red'
+		});
+		$('div.fourth-left').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.second-right').css({
+			'borderColor':'black'
+		});
+		$('div.fourth-left').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
+$('div.third-left').click(function() {
+	if($('div.third-left').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.third-left').css({
+			'borderColor':'red'
+		});
+		$('div.fifth-left').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.third-left').css({
+			'borderColor':'black'
+		});
+		$('div.fifth-left').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
+$('div.third-right').click(function() {
+	if($('div.third-right').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.third-right').css({
+			'borderColor':'red'
+		});
+		$('div.fifth-left').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.third-right').css({
+			'borderColor':'black'
+		});
+		$('div.fifth-left').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
+$('div.fourth-left').click(function() {
+	if($('div.fourth-left').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.fourth-left').css({
+			'borderColor':'red'
+		});
+		$('div.fifth-right').css({
+			'borderRight':'solid 5px red'
+		});
+	} else {
+		$('div.fourth-left').css({
+			'borderColor':'black'
+		});
+		$('div.fifth-right').css({
+			'borderRight':'solid 5px black'
+		});
+	}
+});
+
+$('div.fourth-right').click(function() {
+	if($('div.fourth-right').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.fourth-right').css({
+			'borderColor':'red'
+		});
+		$('div.fifth-right').css({
+			'borderRight':'solid 5px red'
+		});
+	} else {
+		$('div.fourth-right').css({
+			'borderColor':'black'
+		});
+		$('div.fifth-right').css({
+			'borderRight':'solid 5px black'
+		});
+	}
+});
+
+$('div.fifth-left').click(function() {
+	if($('div.fifth-left').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.fifth-left').css({
+			'borderColor':'red'
+		});
+		$('div.winner').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.fifth-left').css({
+			'borderColor':'black'
+		});
+		$('div.winner').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
+$('div.fifth-right').click(function() {
+	if($('div.fifth-right').css("borderColor") != "rgb(255, 0, 0)") {
+		$('div.fifth-right').css({
+			'borderColor':'red'
+		});
+		$('div.winner').css({
+			'borderLeft':'solid 5px red'
+		});
+	} else {
+		$('div.fifth-right').css({
+			'borderColor':'black'
+		});
+		$('div.winner').css({
+			'borderLeft':'solid 5px black'
+		});
+	}
+});
+
