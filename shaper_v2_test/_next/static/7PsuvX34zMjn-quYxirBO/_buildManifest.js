e59b368b50c186188d2e4679731e629759ce6831
Shunnosuke Kataoka <shunnosuke834@gmail.com>
2022-06-11 11:41:19 +0900

    fix

---
+++ shaper_v2_test/_next/static/7PsuvX34zMjn-quYxirBO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/22dd05f7be213a8c.css","static/chunks/pages/index-d8fb3e29fad9a01b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
